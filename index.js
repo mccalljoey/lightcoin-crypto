3c61239ff44c5893df68994ce83654d8617aa5f3
mccalljoey <mccalljoey@gmail.com>
2021-01-16 20:38:34 +0000

    intro to promises

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   }
+};
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
+};
 
-}
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+};
 
-
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+};
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+
+// t1 = new Withdrawal(50.25);
+// t1.commit();
+// console.log('Transaction 1:', t1);
 
 t2 = new Withdrawal(9.99);
 t2.commit();
